6c8393bf16cec5f26bcddf8ec148ba0633359302
Tajana Čolić <74814005+tajanacolic@users.noreply.github.com>
2022-01-31 15:06:05 +0100

    upload

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Post;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::paginate(20);
+
+        return view('posts.index', [
+            'posts' => $posts
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+
+        $this->validate($request, [
+            'body' => 'required'
+        ]);
+
+        $request->user()->posts()->create($request->only('body'));
+
+        return back();
+    }
+}
